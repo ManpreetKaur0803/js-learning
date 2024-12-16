37a548a85a76499187a5626f555d48dac380e761
Manpreet Kaur <112762255+ManpreetKaur0803@users.noreply.github.com>
2024-12-16 20:09:44 +0000

    comparision learning

---
+++ 01_basics/01_variable.js
@@ -0,0 +1,19 @@
+const accountId = 14553;
+let accountEmail = "hitesh@google.com";
+var accountPassword = "12345";
+accountCity = "Jaipur"; //This can happen but prefer not to use
+
+// accountId = 2         not allowed
+
+accountEmail = "h@g.com";
+accountPassword = "212111";
+accountCity = "Bengaluru";
+let accountState;
+
+console.table([
+  accountId,
+  accountEmail,
+  accountPassword,
+  accountCity,
+  accountState,
+]);
---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,23 @@
+"use strict"; // treat all JS code as newer version
+
+// alert(3+3) // we are using nodejs, not browser
+
+let name = "manpreet"
+let age = 21
+let isLoggedIn = true
+
+// number => 2 to power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined => value is still not assigned
+// symbol => unique
+
+// object 
+
+console.log(typeof "manpreet");
+console.log(typeof null);     // object
+console.log(typeof undefined);   // undefined
+
+
